6438ee741857c21c03a22f0244da1516d6d77c09
Stacynsk <130212122+Stacynsk@users.noreply.github.com>
2023-06-15 23:01:39 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+token = '6014645071:AAFD0OlVCPKWj_BiiRg2BXmF_2dgjZiOPjs'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'eur': 'EUR',
+    'usd': 'USD',
+    'rub': 'RUB'
+}
